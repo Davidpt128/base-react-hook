8b94db839ea2e77407124fc5897edc70f7678131
datpham <thanhdat.12081998@gmail.com>
2024-01-16 15:11:51 +0700

    set up base redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { connect } from "react-redux"
 
-const Home = () => {
+import {
+    increaseCounter,
+    decreaseCounter,
+} from "../redux/Counter/counter.actions"
+
+const Home = (props) => {
     return (
-        <div>Home</div>
+        <div><div>Count: {props.count}</div>
+
+            <button onClick={() => props.increaseCounter()}>Increase Count</button>
+
+            <button onClick={() => props.decreaseCounter()}>Decrease Count</button></div>
     )
 }
 
-export default Home
+const mapStateToProps = state => {
+    return {
+        count: state.counter.count,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        increaseCounter: () => dispatch(increaseCounter()),
+
+        decreaseCounter: () => dispatch(decreaseCounter()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
